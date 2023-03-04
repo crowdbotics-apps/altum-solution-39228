45c2dab90c774e2d66563e40513caef219b053df
Crowdbotics <team@crowdbotics.com>
2023-03-04 21:49:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "altum-solution-39228",
+  url: "https://altum-solution-39228.botics.co",
+  api: "https://altum-solution-39228.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
